8b42b3fe82354b8964fe50519619802810c2d7e4
HenriqueBeserra <millerhiquinho@gmail.com>
2022-10-19 09:45:26 -0300

    Algoritmo selction sort

---
+++ Aulas05-Algoritmos/listaDeLivros.js
@@ -0,0 +1,21 @@
+
+const lista1 = [
+    {Livro:"Python", preco: '25.00' },
+    {Livro:"PHP", preco: '22.00'},
+    {Livro:"Go", preco: '29.00'},
+    {Livro:"Javascript", preco: '45.00'},
+    {Livro:"Programando a vida", preco: '81.00'}
+]
+
+const lista2 = [
+    {Livro:"Java", preco: '29.00' },
+    {Livro:"Ruby", preco: '33.00'},
+    {Livro:"Código limpo", preco: '39.00'},
+    {Livro:"Algoritmos", preco: '45.00'},
+    {Livro:"Jogos", preco: '59.00'}
+]
+
+const livros = {lista1, 
+                lista2} 
+
+module.exports = livros;
